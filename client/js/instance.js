0cb3b87901900dc4482089aadf35391d1631238a
白夜 <18556134754@163.com>
2022-08-31 23:19:21 +0800

    some update

---
+++ client/js/instance.js
@@ -0,0 +1,11 @@
+const instance = axios.create()
+
+instance.defaults.baseURL = 'http://127.0.0.1:8888'
+instance.defaults.headers['Content-Type'] = 'multipart/form-data'
+instance.defaults.transformRequest = (data, headers) => {
+  const contentType = headers['Content-Type']
+  if (contentType === 'application/x-www-form-urlencoded') return Qs.stringify(data)
+  return data
+}
+
+instance.interceptors.response.use(response => response.data, err => Promise.reject(err))
